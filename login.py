df6e5855d9777b620bd27904468d25f07fc359aa
zhangjialin <cainiaocome@gmail.com>
2015-01-01 11:26:05 +0800

    version0.2

--- login.py
+++ login.py
@@ -1,49 +1,48 @@
 #!/usr/bin/python
 
 import httplib,urllib2,hashlib
+import requests
 import time
 
-username = '1120122078'
-password = '661230'
+username = 'webadmin'
+password = '888888'
 def md5(mate):
 	m = hashlib.md5()
 	m.update(mate)
 	return m.hexdigest()
-def prepare():
-	hc = httplib.HTTPConnection(host = '10.0.0.55')
-	return hc
 
 def main():
     i = 0;
-    hc = prepare()
-    password1 = md5(password)
-    password2 = password1[8:24]
-    data = "username="+username+"&password="+password2+"&drop="+"0"+"&type=1&n=100"
     headers = {"Content-type":"application/x-www-form-urlencoded",
         'connection':'keep-alive',
         "Accept":"text/plain"}
-    try:
-        hc.request(method = 'POST',url = '/cgi-bin/do_login',body = data,headers = headers)
-        response = hc.getresponse()
-    except: 
-        print 'exception not handled, please contact the author'
-    result = response.read()
-    if result == 'username_error':
-        print 'username wrong'
-    elif result == 'password_error':
-        print 'password wrong'
-    else:
-        print 'login ok'
-        while True:
-            data = 'uid={}'.format(result);
-            hc.request(method = 'POST',url = '/cgi-bin/keeplive',body = data,headers = headers)
-            response = hc.getresponse()
-            print response.read()
-            time.sleep(1)
-            i = i +1;
-            if i>5:
-                break;
-	return 0
+    payload = {'action':'login',
+            'username':'webadmin',
+            'password':'888888',
+            'ac_id':'8',
+            'type':'1',
+            'wbaredirect':'',
+            'mac':'',
+            'user_ip':''}
+    r = requests.post(url='http://10.0.0.55/cgi-bin/srun_portal', data=payload, headers=headers);
+    print r.text
+    #if result == 'username_error':
+    #    print 'username wrong'
+    #elif result == 'password_error':
+    #    print 'password wrong'
+    #else:
+    #    print result
+    #    print 'login ok'
+    #    while True:
+    #        data = 'uid={}'.format(result);
+    #        hc.request(method = 'POST',url = '/cgi-bin/keeplive',body = data,headers = headers)
+    #        response = hc.getresponse()
+    #        print response.read()
+    #        time.sleep(1)
+    #        i = i +1;
+    #        #if i>5:
+    #        #    break;
+	#return 0
 
 if __name__ == '__main__':
 	main()
